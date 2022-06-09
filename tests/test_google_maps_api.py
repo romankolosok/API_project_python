55aa797fa05420cac74de7b56de695264f72559d
kolosok.roman <romakolosok14@gmail.com>
2022-06-08 23:26:31 -0400

    completed all checks for API

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,3 +1,4 @@
+import json
 from requests import Response
 from utils.api import Google_maps_api
 from utils.checking import Checking
@@ -13,32 +14,46 @@ class Test_create_place():
         check_post = result_post.json()
         place_id = check_post.get("place_id")
         Checking.check_status_code(result_post,200)
+        Checking.check_required_data(result_post,['status', 'place_id', 'scope', 'reference', 'id'])
+        Checking.check_required_data_value(result_post,"status","OK")
+
 
         print("POST-GET Method")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get,200)
+        Checking.check_required_data(result_get,['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
+        Checking.check_required_data_value(result_get,"address", "29, side layout, cohen 09")
 
 
         print("PUT Method")
         result_put: Response = Google_maps_api.put_new_place(place_id)
         Checking.check_status_code(result_put,200)
+        Checking.check_required_data(result_put,["msg"])
+        Checking.check_required_data_value(result_put,"msg","Address successfully updated")
 
 
         print("PUT-GET Method")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get,200)
+        Checking.check_required_data(result_get,['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
+        Checking.check_required_data_value(result_get,"address", "858 Roserail Dr, US")
 
 
         print("DELETE Method")
         result_del = Google_maps_api.del_new_place(place_id)
         Checking.check_status_code(result_del,200)
+        Checking.check_required_data(result_del,["status"])
+        Checking.check_required_data_value(result_del,"status", "OK")
 
 
         print("DELETE-GET Method")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get,404)
+        Checking.check_required_data(result_get,["msg"])
+        Checking.check_required_data_value(result_get,"msg", "Get operation failed, looks like place_id  doesn't exists")
 
 
+        print("Creating, Changing, Deletion of new location ended")
 
 
 
--- utils/checking.py
+++ utils/checking.py
@@ -1,3 +1,4 @@
+import json
 from requests import Response
 
 """Methods for requests status checking"""
@@ -12,3 +13,25 @@ class Checking():
             print("SUCCESS , Status code: " + str(response.status_code))
         else:
             print("FAILURE , Status code: " + str(response.status_code))
+
+
+    """Method for required data"""
+    @staticmethod
+    def check_required_data(response: Response,expected_data):
+        token = json.loads(response.text)
+        assert list(token) == expected_data
+        if list(token) == expected_data:
+            print("All required data is present")
+
+
+    """Method for checking values of required data"""
+    @staticmethod
+    def check_required_data_value(response: Response,field_name, expected_value):
+        check = response.json()
+        check_info = check.get(field_name)
+
+        assert expected_value == check_info
+        if expected_value == check_info:
+            print(field_name + " Value is VALID")
+        else:
+            print(field_name + " Value is NOT VALID")
